39dfca8404c9b736d8b05fea5de6ee9237811d64
AntaresBoy <2531646835@qq.com>
2022-03-06 09:40:05 +0800

    feat:单元测试

---
+++ test/demo.test.js
@@ -0,0 +1,15 @@
+/**
+ * @description 单元测试demo
+ * @author AntaresLpq
+ */
+
+
+function twoSum(a,b){
+  return a+b;
+}
+
+//单元测试
+test("10+20 should be equal 30",()=>{
+  const res=twoSum(10,20)
+  expect(res).toBe(30)
+})
